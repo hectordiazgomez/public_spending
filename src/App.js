e86daa7cf346995c4a99dd13605e0c86a8c77eec
hectordiazgomez <konlaptechs@gmail.com>
2023-10-04 12:46:33 -0500

    oct4 commit f

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import {ChartBarIcon, PresentationChartBarIcon} from "@heroicons/react/solid"
 
 function LoadingIcon() {
   return (
@@ -12,26 +13,42 @@ function LoadingIcon() {
 }
 
 function App() {
+
+  const [region, setRegion] = useState(true);
+  const [provincia, setProvincia] = useState(false)
+  const [loadingP, setLoadingP] = useState(false)
+
+ const showR = () => {
+  setRegion(true)
+  setProvincia(false)
+ }
+
+ const showP = () => {
+  setRegion(false)
+  setProvincia(true)  
+ }
+
   const [formattedDate, setFormattedDate] = useState('');
   const [data, setData] = useState([]);
+  const [datap, setDatap] = useState([]);
   const [loading, setLoading] = useState(true); 
 
   const fetchData = async () => {
     try {
-      const response = await axios.get('http://localhost:3000/data', {
-        timeout: 30000,
-      });
-      setData(response.data);
-      setLoading(false); 
+        const response = await axios.get('http://127.0.0.1:5000/getdata', {
+            timeout: 20000,
+        });
+        setData(response.data.region);
+        setDatap(response.data.province);
+        setLoading(false);
     } catch (error) {
-      console.error('Error fetching data:', error);
-      setLoading(false); 
+        console.error('Error fetching data from MongoDB:', error);
+        setLoading(false);
     }
-  };
+};
 
   useEffect(() => {
     document.title = 'Ejecución presupuestal amigable';
-
     fetchData();
     const currentDate = new Date();
     const options = { day: '2-digit', month: 'short', year: 'numeric' };
@@ -46,35 +63,76 @@ function App() {
   return (
     <div className="bg-gray-100 min-h-screen py-8">
       <div className="text-center">
-        <h1 className="text-4xl font-semibold text-blue-600 mb-8">
+        <p className="text-3xl font-semibold text-blue-600 mb-8">
           Avance de Ejecución Presupuestal
-        </h1>
-        <p className="text-gray-600 font-medium">
-          Información actualizada al día {formattedDate}
         </p>
       </div>
-      <div className="flex justify-center mt-8">
-        <div className="w-5/6 sm:w-4/5">
-          {loading ? (
-            <div className='flex justify-center'>
-              <LoadingIcon /> 
-            </div>
-          ) : (
-            <ul>
-              {data.map((item, index) => (
-                <li className="my-6" key={index}>
-                  <p className="text-gray-800 text-lg mb-2">
-                    {index + 1}. {removePrefix(item.name)}: {item.avance}%
-                  </p>
-                  <ProgressBar
-                    percentage={item.avance}
-                    isAmazonas={item.name.includes('AMAZONAS')}
-                  />
-                </li>
-              ))}
-            </ul>
-          )}
+      <div className='flex justify-center'>
+      <div className='w-4/5'>
+      <p className="text-gray-600">
+          Información actualizada al día {formattedDate} a las 9:00 am (Hora de Lima)
+        </p>
+      </div>
+      </div>
+      <div className='flex justify-center'>
+        <div className='w-4/5 my-12 sm:w-1/2 flex justify-around'>
+          <div onClick={showR} className={`${region? "text-purple-600" : "text-blue-600 hover:text-purple-500 cursor-pointer" } flex`}>
+            <ChartBarIcon className='w-7 h-auto mr-2' />
+            <p className='text-xl font-medium'>Regiones</p>
+          </div>
+          <div onClick={showP} className={`${provincia? "text-purple-600" : "text-blue-600 hover:text-purple-500 cursor-pointer" } flex`}>
+            <PresentationChartBarIcon className='w-7 h-auto mr-2' />
+            <p className='text-xl font-medium'>Provincias</p>
+          </div>
         </div>
+      </div>
+      <div className="flex justify-center mt-8">
+          {region &&
+                  <div className="w-5/6 sm:w-4/5">
+                  {loading ? (
+                    <div className='flex justify-center'>
+                      <LoadingIcon /> 
+                    </div>
+                  ) : (
+                    <ul>
+                      {data.map((item, index) => (
+                        <li className="my-6" key={index}>
+                          <p className="text-gray-800 text-lg mb-2">
+                            {index + 1}. {removePrefix(item.name)}: {item.avance}%
+                          </p>
+                          <ProgressBar
+                            percentage={item.avance}
+                            isAmazonas={item.name.includes('AMAZONAS')}
+                          />
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+          }
+          {provincia &&
+                  <div className="w-5/6 sm:w-4/5">
+                  {loadingP ? (
+                    <div className='flex justify-center'>
+                      <LoadingIcon /> 
+                    </div>
+                  ) : (
+                    <ul>
+                      {datap.map((item, index) => (
+                        <li className="my-6" key={index}>
+                          <p className="text-gray-800 text-lg mb-2">
+                            {index + 1}. {item.name}: {item.avance}%
+                          </p>
+                          <ProgressBar
+                            percentage={item.avance}
+                            isAmazonas={item.name.includes('AMAZONAS')}
+                          />
+                        </li>
+                      ))}
+                    </ul>
+                  )}
+                </div>
+          }
       </div>
     </div>
   );
