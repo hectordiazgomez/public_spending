2692b37dcc510cda050aaef99f0f9e8b7e7329e8
hectordiazgomez <konlaptechs@gmail.com>
2023-11-15 10:48:17 -0500

    fixed ui

--- src/App.js
+++ src/App.js
@@ -38,16 +38,30 @@ function App() {
   setDistrito(true)   
  }
 
+  const [popUp, setPopUp] = useState(false);
+
+ const toggle = () => {
+   setPopUp(!popUp)
+ }
+
+  const [popUp2, setPopUp2] = useState(false);
+
+  const toggle2 = () => {
+    setPopUp2(!popUp2)
+  }
+
   const [formattedDate, setFormattedDate] = useState('');
   const [data, setData] = useState([]);
   const [datap, setDatap] = useState([]);
-  const [datad, setDatad] = useState([])
-  const [loading, setLoading] = useState(true); 
+  const [datad, setDatad] = useState([]);
+  const [districtsProjects, setDistrictProjects] = useState([])
+  const [loading, setLoading] = useState(false); 
 
   const fetchData = async () => {
+    setLoading(true);
     try {
-      const response = await axios.get('https://awsdata.konlaptechs.com:9020/getdatall', {
-            timeout: 20000,
+      const response = await axios.get('http://localhost:5000/getdatall', {
+            timeout: 100000,
         });
         setData(response.data.region);
         setDatap(response.data.province);
@@ -60,14 +74,35 @@ function App() {
 };
 
   useEffect(() => {
-    document.title = 'Ejecución presupuestal amigable';
-    fetchData();
-    const currentDate = new Date();
-    const options = { day: '2-digit', month: 'short', year: 'numeric' };
-    const formatted = currentDate.toLocaleDateString('en-US', options);
-    setFormattedDate(formatted);
-    console.log("Holaaaa")
-  }, []);
+    fetchData()
+  }, [])
+
+const districtData = async () => {
+  try {
+    const answer = await axios.get(`http://localhost:5000/getdatadistrict`, {
+      timeout: 100000,
+    });
+    setDistrictProjects(answer.data.distritox);
+    toggle();
+    console.log(answer.data.distritox)
+  } catch (error) {
+    console.error('Error fetching data from MongoDB:', error);
+    setLoading(false);
+  }
+}
+  const [provinceProjects, setProvinceProjects] = useState([])
+  const provinceData = async () => {
+    try {
+      const answer = await axios.get(`http://localhost:5000/getdataprovince`, {
+        timeout: 100000,
+      });
+      setProvinceProjects(answer.data.provincex);
+      console.log(answer.data.provincex)
+    } catch (error) {
+      console.error('Error fetching data from MongoDB:', error);
+    }
+  }
+
 
   const removePrefix = (name) => {
     return name.replace(/^(464: GOBIERNO REGIONAL DE LA PROVINCIA CONSTITUCIONAL DEL |465: |[\d]+: GOBIERNO REGIONAL DEL DEPARTAMENTO DE )\s*/, '');
@@ -136,7 +171,7 @@ function App() {
                   ) : (
                     <ul>
                       {datap.map((item, index) => (
-                        <li className="my-6" key={index}>
+                        <li className="my-6" key={index} onClick={provinceData} >
                           <p className="text-gray-800 text-lg mb-2">
                             {index + 1}. {item.name}: {item.avance}%
                           </p>
@@ -159,7 +194,7 @@ function App() {
                   ) : (
                     <ul>
                       {datad.map((item, index) => (
-                        <li className="my-6" key={index}>
+                        <li className="my-6 cursor-pointer" key={index} onClick={() => districtData(item.name)}>
                           <p className="text-gray-800 text-lg mb-2">
                             {index + 1}. {item.name}: {item.avance}%
                           </p>
@@ -173,6 +208,8 @@ function App() {
                   )}
                 </div>
           }
+        {popUp && <Projects toggle={toggle} districtsProjects={districtsProjects} />}
+        {popUp2 && <ProjectsProvince toggle2={toggle2} provinceProjects={provinceProjects} />}
       </div>
     </div>
   );
@@ -189,6 +226,108 @@ const ProgressBar = ({ percentage, isAmazonas }) => {
     <div className={progressBarClassName}>
       <div
         className={isAmazonas ? 'h-full bg-red-500 rounded-full' : 'h-full bg-blue-500 rounded-full'}
+        style={{ width: `${percentage}%` }}
+      ></div>
+    </div>
+  );
+};
+
+const ProjectsProvince = ({ provinceProjects, toggle2 }) => {
+
+  return (
+    <div className=''>
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity">
+          </div>
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <div
+            className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:w-4/5"
+            role="dialog"
+            aria-labelledby="modal-headline"
+          >
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              {provinceProjects?.map((item, index) => (
+                <li className="my-6" key={index}>
+                  <p className="text-gray-800 text-lg my-2">
+                    {item.projectname} - {item.PIM} - {item.CERTIFICACION}
+                  </p>
+                  <ProgressBard percentage={parseInt(item.PORCENTAJE_AVANCE)} />
+                </li>
+              ))}
+            </div>
+            <div className="bg-gray-50 px-4 mt-6 sm:mt-0 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                className="w-full inline-flex justify-center rounded-md border border-gray-300 px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={toggle2}
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+const Projects = ({ districtsProjects, toggle }) => {
+
+  return (
+    <div className=''>
+      <div className="fixed z-10 inset-0 overflow-y-auto">
+        <div className="flex items-center justify-center min-h-screen pt-4 px-4 pb-20 text-center sm:block sm:p-0">
+          <div className="fixed inset-0 transition-opacity">
+          </div>
+          <span
+            className="hidden sm:inline-block sm:align-middle sm:h-screen"
+            aria-hidden="true"
+          >
+            &#8203;
+          </span>
+          <div
+            className="inline-block align-bottom bg-white rounded-lg text-left overflow-hidden transform transition-all sm:my-8 sm:align-middle sm:w-4/5"
+            role="dialog"
+            aria-labelledby="modal-headline"
+          >
+            <div className="bg-white px-4 pt-5 pb-4 sm:p-6 sm:pb-4">
+              {districtsProjects?.map((item, index) => (
+                <li className="my-6" key={index}>
+                  <p className="text-gray-800 text-lg my-2">
+                    {item.projectname} - {item.PIM} - {item.CERTIFICACION}
+                  </p>
+                  <ProgressBard percentage={parseInt(item.PORCENTAJE_AVANCE)} />
+                </li>
+              ))}
+            </div>
+            <div className="bg-gray-50 px-4 mt-6 sm:mt-0 py-3 sm:px-6 sm:flex sm:flex-row-reverse">
+              <button
+                type="button"
+                className="w-full inline-flex justify-center rounded-md border border-gray-300 px-4 py-2 bg-white text-base font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500 sm:ml-3 sm:w-auto sm:text-sm"
+                onClick={toggle}
+              >
+                Cerrar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const ProgressBard = ({ percentage }) => {
+  return (
+    <div className="h-4 w-full bg-gray-200 rounded-full">
+      <div
+        className="h-full bg-blue-500 rounded-full"
         style={{ width: `${percentage}%` }}
       ></div>
     </div>
